9675b7847ab6d07fe50f6bed9c5ebdefbc22c289
Gmail <77217686+G-MA1L@users.noreply.github.com>
2021-12-03 08:49:26 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,9 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6285795431803', 'Andy', m)
-  conn.sendContact(m.chat, '6289528232401', 'Zul', m)
+  conn.sendContact(m.chat, '6283805279906', 'Ismail Kurama', m)
 conn.reply(m.chat, `@${global.owner[0]}
 `.trim(), m, { contextIfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
-conn.reply(m.chat, `wa.me/6289528232401`, m)
+conn.reply(m.chat, `wa.me/6283805279906`, m)
 }
 
 handler.command = /^owner|creator$/i
